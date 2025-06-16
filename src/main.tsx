dfb9ad81d7a98ab710669c3b2698c8f034d184a1
kronoron <kronnfeldt89@hotmail.com>
2025-06-16 23:40:32 +1000

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,1 @@
+tsx<br>import React from "react";<br>import ReactDOM from "react-dom/client";<br>import App from "./App";<br><br>ReactDOM.createRoot(document.getElementById("root")!).render(<App />);<br>
